871cda5a44faaff2fc4422607d9e1fd8de73cb0d
adamc66 <adam.cote66@gmail.com>
2019-08-01 10:01:57 -0400

    copied files from django blog part 4

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length = 255, null=False)
+    body = models.TextField(blank = True, null=False)
+    draft = models.BooleanField()
+    published_date = models.DateField()
+    author = models.CharField(max_length = 63, null = False)
+    
+    def __str__(self):
+        return (f'{self.title}, By: {self.author}')
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from blog.models import Article, Comment
+
+def root(request):
+    return HttpResponseRedirect('home')
+
+def home(request):
+    context = {'date' : datetime.today().strftime('%Y-%m-%d'), 
+    'articles': Article.objects.all()}
+    response = render(request, 'index.html', context)
+    return HttpResponse(response)
+
+
+def article(request, id):
+    context = {'article': Article.objects.get(pk=id)} 
+    return render(request,'article.html', context)
+
+def create_comment(request):
+    article_id = request.POST['article']
+
+    articlelink = Article.objects.filter(id=article_id).first()
+    name = request.POST['name']
+    message = request.POST['message']
+
+    new_comment = Comment(name=name, message=message, article=articlelink)
+    new_comment.save()
+
+    context = {'article': articlelink}
+    return render(request,'article.html', context)
